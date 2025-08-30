b6a845c91bb7e1afa84ef2e386635b10173e4608
ayafullcrit <nguyendangphong2k6@gmail.com>
2025-08-30 08:40:00 +0700

    Update User.hpp

--- User.hpp
+++ User.hpp
@@ -7,19 +7,18 @@ class User{
         string AccountName;
         string UserName;
         string Password;
-        string location;
+        string Location;
         int AccountBalance;
     public:
         User();
         User(const string& role, const string& accountName, const string& userName, const string& password,const string& location, const int& accountBalance);
         void Register();
         void Login();
+        void Logout();
         void ShowInfo();
-        string GetLocation() const { return Location; }
+        void Update_UserName();
+        void Update_Location();
+        string GetLocation() const { return this->Location ; }
         string GetName() const { return this->UserName; }
-        int GetBalance() const { return AccountBalance; }
-        // bool IsExisted();
-        // bool IsValid();
-
+        int GetBalance() const { return this->AccountBalance; }
 };
-
