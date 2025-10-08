2fb25fb74d3f28bc7da90bc7b38c1dbd1d3091ef
dactri2205 <dactri641@gmail.com>
2025-10-08 11:20:21 +0700

    Student.h

--- Student.h
+++ Student.h
@@ -4,47 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include"Username.hpp"
+#include"User.hpp"
 using namespace std;
-
-class Teacher : public User{//ke thua tu User
-	private:
-		vector<Student> StudentList;
-		vector<Subject> SubjectList;	
-	//ƠU
-	public:
-		
-		void AddSubject();
-		void Update_SubjectInfo();
-		void DeleteSubJect();
-		
-		void Show_StudentList();// xem danh sach hoc sinh dang theo hoc
-		void Find_NewStudent();
-		void Find_ByLocation();
-		void AddStudent();
-		void DeleteStudent();
-	
-		void Update_Schedule();	
-		
-		void Get_TeachingFee();
-		vector <Subject> Getsubject(){
-			return this-> SubjectList;
-		}
-		
-};
-
-class Subject{
-	private: 
-	unsigned int Fee;
-	string Name;
-	vector<string> Session;
-	float rate;
-	public:
-	string Getname(){
-		return this->Name;
-	}
-};
-
 
 class Student : public User {
 private:
@@ -53,13 +14,15 @@ private:
 public:
     Student(string accName = "", string pass = "", string uname = "", string id = "", string loc = "", int balance = 0);
 
-    void AddTeacher(Teacher t);
+    void AddTeacher(const Teacher& t);
     void Show_TeacherList() ;
 
     void Find_BySubject(string& subjectName);
     void Find_ByLocation( string& location) ;
 
-    void Pay_TeachingFee(Teacher* t, int amount);
+    void Pay_TeachingFee(Teacher t, int amount);
     void FeedBack();
+	friend class Subject;
+	friend class Teacher;
 };
-
+// code mỗi class 1 file
