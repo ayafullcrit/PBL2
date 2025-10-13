ffc71dc98a619bcca101400086b469795cbe31ab
dactri2205 <dactri641@gmail.com>
2025-10-13 17:11:12 +0700

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,33 +1,32 @@
-#include<iostream>
-#include"User.hpp"
+#pragma once
+#include <iostream>
 #include <vector>
-#include"Student.h"
-class Teacher : public User{//ke thua tu User
-	private:
-		vector<Student> StudentList;
-		vector<Subject> SubjectList;	
-	//ƠU
-	public:
-		//friend Update_AccountInfo()
-		
-		void AddSubject(const Subject&);
-		void Update_SubjectInfo();
-		void DeleteSubJect();
-		
-		void Show_StudentList();// xem danh sach hoc sinh dang theo hoc
-		void Find_NewStudent();
-		void Find_ByLocation();
-		void AddStudent(const Student& );
-		void DeleteStudent();
-        friend ostream& operator <<(ostream&, const Student&);
-        friend istream& operator >>(istream&, Student&);
-        friend class Subject;
-		vector<Subject> GetSubjectList() const;
+#include "User.hpp"
+#include "Student.h"
 
-        
-		
-		//friend Show_Schedule();
-		//Update_Schedule();	
-		
-		//Get_TeachingFee();
+
+using namespace std;
+class Student;
+class Teacher : public User { //ke thua tu User
+private:
+    Student a;
+    vector<Student> StudentList;
+    vector<Subject> SubjectList;    
+public:
+    //friend Update_AccountInfo()
+    Teacher(string accName = "", string pass = "", string uname = "", string id = "", string loc = "", int balance = 0);
+
+    void AddSubject(const Subject&);
+    void Update_SubjectInfo();
+    void DeleteSubJect();
+    
+    void Show_StudentList();// xem danh sach hoc sinh dang theo hoc
+    void Find_NewStudent();
+    void Find_ByLocation();
+    void AddStudent(const Student& );
+    void DeleteStudent();
+    friend ostream& operator <<(ostream&, const Student&);
+    friend istream& operator >>(istream&, Student&);
+    friend class Subject;
+    vector<Subject> GetSubjectList() const;
 };
