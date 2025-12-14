e8c381fc6186491c2b8e82d9c6a133d0580ec43b
dactri2205 <dactri641@gmail.com>
2025-12-14 17:50:51 +0700

    Delete updateadmin directory

--- updateadmin/AdminScreen.h
+++
@@ -1,8 +0,0 @@
-class Admin;
-void RemoveUser(Admin* admin);
-void ResetPassword(Admin* admin);
-void ApproveTutor(Admin*);
-Admin* LoginAdmin();
-void ShowAdminMenu();
-void HandleAdminMenu(Admin* admin);
-void DisplayUserList();
